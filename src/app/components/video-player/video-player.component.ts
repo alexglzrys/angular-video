61546e73c0a8e4c6883cd6dd65570b384b0186cf
Alejandro González Reyes <alexglzrys@gmail.com>
2021-10-24 11:17:54 -0500

    programar controles de reproducción de video

--- src/app/components/video-player/video-player.component.ts
+++ src/app/components/video-player/video-player.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { IVideo } from 'src/app/interfaces/video';
 import { VideosService } from 'src/app/services/videos.service';
@@ -10,8 +10,12 @@ import { VideosService } from 'src/app/services/videos.service';
 })
 export class VideoPlayerComponent implements OnInit {
 
+  @ViewChild('videoPlayer') player!: ElementRef;
   video!: IVideo
   id!: number
+  duration!: string
+  current!: string
+  progreso!: number
 
   constructor(private route: ActivatedRoute,
               private videosService: VideosService) { }
@@ -35,4 +39,41 @@ export class VideoPlayerComponent implements OnInit {
     const URL_SERVER = 'http://localhost:8888/Backend/services-angular-videos/uploads/videos/';
     return `${URL_SERVER}${videoName}`;
   }
+
+  play() {
+    this.player.nativeElement.play()
+  }
+
+  pause() {
+    this.player.nativeElement.pause();
+  }
+
+  stop() {
+    this.player.nativeElement.pause();
+    this.player.nativeElement.currentTime = 0
+  }
+
+  // Obtener metainformaciòn del video referenciado en el elemento video de HTML
+  onMetadata(event: Event, video: HTMLVideoElement) {
+    console.log(video)
+    let minutos = Math.floor(video.duration / 60);
+    let segundos = Math.floor(video.duration) - (minutos * 60);
+    let minStr = ''+minutos, secStr=''+segundos;
+    if (minutos < 10) minStr = 0 + '' + minutos
+    if (segundos < 10) secStr = 0 + '' + segundos;
+    this.duration = minStr + ':' + secStr;
+  }
+
+  // Obtener información en tiempo real del estado de reproducción del video
+  onTimeUpdate(event: Event, video:HTMLVideoElement) {
+    this.progreso = Math.floor(video.currentTime * 100 / video.duration)
+
+    let minutos = Math.floor(video.currentTime / 60);
+    let segundos = Math.floor(video.currentTime) - (minutos * 60);
+    let minStr = ''+minutos, secStr=''+segundos;
+    if (minutos < 10) minStr = 0 + '' + minutos
+    if (segundos < 10) secStr = 0 + '' + segundos;
+    this.current = minStr + ':' + secStr;
+  }
+
 }
